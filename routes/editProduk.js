c4a1add3f84edfb2381a6a8b0cb2baba29fbf21b
Vega Anggara Saputra <117183001+Yaduhh@users.noreply.github.com>
2024-07-10 13:35:01 +0700

    updaeted

--- routes/editProduk.js
+++ routes/editProduk.js
@@ -4,11 +4,11 @@ const db = require("../db");
 
 router.put("/:id", (req, res) => {
   const id = req.params.id;
-  const { name, detail, price } = req.body;
+  const { name, detail, price, status } = req.body;
 
   const updateQuery =
-    "UPDATE menu SET name = ?, detail = ?, price = ? WHERE id = ?";
-  db.query(updateQuery, [name, detail, price, id], (err, result) => {
+    "UPDATE menu SET name = ?, detail = ?, price = ?, status = ? WHERE id = ?";
+  db.query(updateQuery, [name, detail, price, status, id], (err, result) => {
     if (err) {
       console.log(err);
       return res.status(500).send("Error updating menu item");
--- routes/login.js
+++ routes/login.js
@@ -2,25 +2,39 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const db = require("../db");
+const md5 = require("md5");
 
 const SECRET_KEY = "Nrp5kvfBiy1mjd8YZSxwQgajqbwukwFo";
 
 router.post("/", (req, res) => {
   const { username, password } = req.body;
+  const md5Password = md5(password);
 
   const sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-  db.query(sql, [username, password], (err, result) => {
+  db.query(sql, [username, md5Password], (err, result) => {
     if (err) {
       console.error(err);
       return res.status(500).send("Internal server error");
     }
 
     if (result.length > 0) {
-      // Buat token JWT
-      const token = jwt.sign({ username: username }, SECRET_KEY, {
-        expiresIn: "1d",
+      const user = result[0];
+      const { username, name, id } = user;
+      const token = jwt.sign(
+        { username: username, name: name, id: id },
+        SECRET_KEY,
+        {
+          expiresIn: "1d",
+        }
+      );
+      res.json({
+        success: true,
+        message: "Login berhasil",
+        token: token,
+        name: name,
+        username: username,
+        id: id,
       });
-      res.json({ success: true, message: "Login berhasil", token: token });
     } else {
       res.json({ success: false, message: "Username atau password salah" });
     }
